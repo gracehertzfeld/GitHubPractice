59c8e2ae49dadc1f20b248d26a05659ac081b415
grace <grace@GraceDell.wifi.uc.edu>
2022-10-18 08:50:01 -0400

    Added print

--- GitHubPractice/HelloWorldPackage/HelloWorld.py
+++ GitHubPractice/HelloWorldPackage/HelloWorld.py
@@ -1,3 +1,8 @@
 #GitHub Practice code
 
 print("Hello World")
+
+#window --> view --> other --> git --> git staging
+#always do commit & push, not just commit
+
+print("Indiana Basketball is the best")
